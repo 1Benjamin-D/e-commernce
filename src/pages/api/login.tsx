e9538bbd32ebeca2a9cc671965f3efdf3a19d1ab
Gabinks <gabin.buignet@gmail.com>
2024-04-05 14:57:43 +0200

    Refactored the code.

--- src/pages/api/login.tsx
+++ src/pages/api/login.tsx
@@ -1,35 +1,45 @@
 import {NextApiRequest, NextApiResponse} from "next";
 import prisma from "@/libs/prismadb";
 import bcrypt from 'bcryptjs'
+import {cryptPassword} from "@/utils/bcrypt";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    if (req.method === "POST") {
-        const {username, password} = JSON.parse(req.body);
-        if (username && password){
-            const accountExist = await prisma.utilisateur.findFirst({
-                where: {
-                    name: username
-                }
-            })
-            if (accountExist){
-                bcrypt.compare(password, accountExist.password, function(err, result) {
-                    if(result){
-                        res.status(200).json({success: true, message: "Connexion réussite."})
-                    }
-                    else{
-                        res.status(401).json({success: false, message: "Nom d'utilisateur ou mot de passe invalide."})
-                    }
-                });
-            }
-            else{
-                res.status(401).json({success: false, message: "Nom d'utilisateur ou mot de passe invalide."})
-            }
-        }
-        else{
-            res.status(400).json({success: false, message: "Veuillez remplir tout les champs."})
-        }
-    } else {
+    if (req.method !== "POST") {
         res.setHeader('Method-Allowed', 'POST');
-        res.status(405).json({success: false, message: "Method Not Allowed"})
+        return res.status(405).json({success: false, message: "Method Not Allowed"});
     }
+
+    const {username, password} = JSON.parse(req.body);
+
+    // Vérifier si la clé API est correcte
+    const apiKey = await cryptPassword(process.env.API_KEY!);
+    const apiKeyCorrect = await bcrypt.compare(process.env.API_KEY!, apiKey);
+    if (!apiKeyCorrect) {
+        return res.status(403).json({success: false, message: "Forbidden."});
+    }
+
+    // Vérifier si les champs requis sont présents
+    if (!username || !password) {
+        return res.status(400).json({success: false, message: "Veuillez remplir tout les champs."});
+    }
+
+    // Vérifier si le compte utilisateur existe
+    const accountExist = await prisma.utilisateur.findFirst({
+        where: {
+            name: username
+        }
+    });
+
+    if (!accountExist) {
+        return res.status(401).json({success: false, message: "Nom d'utilisateur ou mot de passe invalide."});
+    }
+
+    // Vérifier si le mot de passe est correct
+    bcrypt.compare(password, accountExist.password, function(err, result) {
+        if (result) {
+            return res.status(200).json({success: true, message: "Connexion réussie."});
+        } else {
+            return res.status(401).json({success: false, message: "Nom d'utilisateur ou mot de passe invalide."});
+        }
+    });
 }
