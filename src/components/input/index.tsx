3b58c5009d02f0f8392a173623861083d5f29878
Gabin <gabin.buignet@gmail.com>
2024-04-08 13:36:05 +0200

    Changed the size of the font

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -11,7 +11,7 @@ interface CustomInput {
 export function CustomInput({type, name, labelText, value, onChange, error}: CustomInput) {
     return (
         <div className="flex flex-col justify-center items-center gap-4">
-            <label htmlFor={name} className="text-2xl mobile:text-lg text-center">{labelText}</label>
+            <label htmlFor={name} className="text-lg mobile:text-lg text-center">{labelText}</label>
             <div className="rounded-lg bg-gradient-to-br from-[#FF5863] via-[#FD8F50] to-[#FFC53E] p-px">
                 <input type={type} id={name} name={name} value={value} onChange={onChange} autoComplete="off" className="p-1 bg-gray-300 text-center rounded-[calc(0.5rem-1px)]"/>
             </div>
