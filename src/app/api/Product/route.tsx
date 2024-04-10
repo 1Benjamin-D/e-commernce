5c2ea0acdece6ce288a2519ebe9598980bb4c8c5
Candidat-005 <g.thomas@laposte.net>
2024-04-10 15:44:28 +0200

    Panier

--- src/app/api/Product/route.tsx
+++ src/app/api/Product/route.tsx
@@ -2,6 +2,6 @@ import { NextRequest, NextResponse } from "next/server";
 import getProductByName from "../getProductByName";
 
 export async function GET(request: NextRequest) {
-    const phone = await getProductByName (request)!;
-    return NextResponse.json(phone);
+    const product = await getProductByName (request)!;
+    return NextResponse.json(product);
 }
--- src/app/api/getProductByName.tsx
+++ src/app/api/getProductByName.tsx
@@ -3,6 +3,17 @@ import { Product } from "@prisma/client";
 import { NextRequest } from "next/server";
 
 export default async function getProductByName(request: NextRequest): Promise<Product | null>{
-    const name = request.nextUrl.searchParams.get("name")!;
-    return prisma.product.findFirst({where: {product_name: name}});
+    const name = request.nextUrl.searchParams.get("name");
+    console.log(`Recherche du produit avec le nom: ${name}`);
+
+    // Vérifiez si `name` est null ou vide
+    if (!name) {
+        console.log('Le nom du produit est vide ou manquant.');
+        return null;
+    }
+
+    const product = await prisma.product.findFirst({where: {product_name: name}});
+    console.log(`Produit trouvé:`, product);
+
+    return product;
 }
