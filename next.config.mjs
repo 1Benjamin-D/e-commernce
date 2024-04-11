c5d556f834f6254d61acd2324ee82f503feb3732
1Benjamin-D <desmet.benjamin20@orange.fr>
2024-04-11 14:44:22 +0200

    base

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    domains: ['cdn.auchan.fr'],
     remotePatterns: [
       {
         protocol: 'https',
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,32 @@
-
+'use client'
+import React, { useState } from "react";
 import Navbar from "@/components/Navbar";
+import Input from "@/components/ui/Input";
+import Image from "next/image";
+import Products from "./products/page";
+import Footer from "@/components/Footer";
+import Filters from "@/components/Filters";
 
-export default function Home() {
+const Home: React.FC = () => {
+  const [selectedType, setSelectedType] = useState<string | null>(null);
+
+  const handleTypeChange = (selectedType: string | null) => {
+    setSelectedType(selectedType);
+  };
+
   return (
     <main className="">
       <Navbar />
+      <Filters onTypeChange={handleTypeChange}/>
+      <div className="lg:hidden">
+        <Input>
+          <Image src="/Images/Search-logo.png" alt="search_logo" width={1000} height={1000} className="w-7 h-7" />
+        </Input>
+      </div>
+      <Products/>
+      <Footer />
     </main>
   );
-}
+};
+
+export default Home;
