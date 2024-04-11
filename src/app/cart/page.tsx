1c42fa1c7b1d5425c6dc2c7271ae38ab5865f9e3
Candidat-005 <g.thomas@laposte.net>
2024-04-11 11:04:18 +0200

    Panier

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Image from "next/image";
 
 type CartItemProps = {
@@ -7,6 +7,7 @@ type CartItemProps = {
   name: string;
   description: string;
   price: number;
+  image: string | null;
   onRemove: (id: number) => void;
 };
 
@@ -15,13 +16,13 @@ const CartItem: React.FC<CartItemProps> = ({
   name,
   description,
   price,
+  image,
   onRemove,
 }) => {
-  
   return (
     <div className="flex items-center justify-between p-4 border-b border-gray-300">
       <div className="flex flex-col h-full w-fit bg-gradient-to-r from-[#FF5863] via-[#FD8F50] to-[#FFC53E] p-0.5 rounded-xl">
-        <Image className="w-24 h-auto" src={""} alt={name} />
+        {image && <Image src={image} alt={name} width={96} height={96} />}
         <div className="flex-grow mx-4">
           <p className="text-lg font-bold">{name}</p>
           <p className="text-gray-700">{description}</p>
@@ -41,32 +42,44 @@ const CartItem: React.FC<CartItemProps> = ({
 const Cart: React.FC = () => {
   const [items, setItems] = useState<CartItemProps[]>([]);
 
-  const totalPrice = items.reduce((total, item) => total + item.price, 0);
+  useEffect(() => {
+    const loadProducts = async () => {
+      try {
+        const response = await fetch(`/api/getProduct`);
+        if (!response.ok) {
+          throw new Error('Problème lors de la récupération des données du panier');
+        }
+        const products = await response.json();
+        const newItems = products.map((product: { id: any; name: any; description: any; price: any; image: any; }) => ({
+          id: product.id,
+          name: product.name, 
+          description: product.description,
+          price: product.price,
+          image: product.image,
+          onRemove: removeItemFromCart,
+        }));
+        setItems(newItems);
+      } catch (error) {
+        console.error("Erreur lors du chargement des produits :", error);
+
+      }
+    };
+  
+    loadProducts();
+  }, []);
 
   const removeItemFromCart = (id: number) => {
-    console.log(`Suppression de l'article avec l'id: ${id}`);
-    setItems((currentItems) => {
-      const updatedItems = currentItems.filter((item) => item.id !== id);
-      console.log("Nouvel état du panier après suppression:", updatedItems);
-      return updatedItems;
-    });
+    setItems((currentItems) => currentItems.filter((item) => item.id !== id));
   };
+
+  const totalPrice = items.reduce((total, item) => total + item.price, 0);
 
   return (
     <div className="flex flex-col justify-center items-center space-y-4 min-h-screen bg-white">
       <div className="container mx-auto my-8">
         <div className="shadow-lg rounded-lg">
           {items.length > 0 ? (
-            items.map((item) => (
-              <CartItem
-                key={item.id}
-                id={item.id}
-                name={item.name}
-                description={item.description}
-                price={item.price}
-                onRemove={removeItemFromCart}
-              />
-            ))
+            items.map((item) => <CartItem key={item.id} {...item} />)
           ) : (
             <p className="text-center p-4">Votre panier est vide.</p>
           )}
