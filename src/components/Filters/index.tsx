2bb7b8249592418853da424b0e96f09c50ca05e7
1Benjamin-D <desmet.benjamin20@orange.fr>
2024-04-13 13:51:31 +0200

    usable filters

--- src/components/Filters/index.tsx
+++ src/components/Filters/index.tsx
@@ -1,98 +1,96 @@
 'use client'
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 
-interface FiltersProps {
-  onTypeChange: (selectedType: string | null) => void;
+interface Category {
+  id: number;
+  name: string;
+  image: string;
+  id_sub_cat: { Sub_category: SubCategory }[];
 }
 
-const Filters: React.FC<FiltersProps> = ({ onTypeChange }) => {
-  const [categories, setCategories] = useState<any[]>([]);
-  const [selectedCategory, setSelectedCategory] = useState<string | null>(null);
+interface SubCategory {
+  id: number;
+  name: string;
+  image: string;
+}
+
+interface FiltersProps {
+  onCategoryChange: (categoryId: number | null) => void;
+  onSubCategoryChange: (subCategoryId: number | null) => void;
+}
+
+const Filters: React.FC<FiltersProps> = ({ onCategoryChange, onSubCategoryChange }) => {
+  const [data, setData] = useState<Category[]>([]);
+  const [loaded, setLoaded] = useState(false);
+  const [isClicked, setIsClicked] = useState(false);
+  const [selectedCategory, setSelectedCategory] = useState<number | null>(null);
+  const [selectedSubCategory, setSelectedSubCategory] = useState<number | null>(null);
 
   useEffect(() => {
-    const fetchCategories = async () => {
+    const fetchData = async () => {
       try {
         const response = await fetch('/api/category');
-
-        if (response.ok) {
-          const data: any[] = await response.json();
-          setCategories(data);
-        }
+        const json: Category[] = await response.json();
+        setData(json);
+        setLoaded(true);
       } catch (error) {
         console.error('Error fetching categories:', error);
       }
     };
-
-    fetchCategories();
+    fetchData();
   }, []);
 
-  const handleCategoryChange = async (categoryId: string) => {
-    if (categoryId === selectedCategory) {
-      setSelectedCategory(null);
-      onTypeChange(null);
-    } else {
-      try {
-        const response = await fetch(`/api/category?id_cat=${categoryId}`);
-
-        if (response.ok) {
-          const data: any[] = await response.json();
-          setCategories(data);
-          setSelectedCategory(categoryId);
-          onTypeChange(null);
-        } else {
-          console.error('Error fetching subcategories - response not OK');
-        }
-      } catch (error) {
-        console.error('Error fetching subcategories:', error);
-      }
-    }
+  const handleCategoryClick = (categoryId: number) => {
+    setIsClicked(!isClicked);
+    setSelectedCategory(categoryId);
+    setSelectedSubCategory(null);
+    onCategoryChange(categoryId);
   };
 
-  const handleSubCategoryChange = (subCategoryId: string) => {
-    onTypeChange(subCategoryId === selectedCategory ? null : subCategoryId);
+  const handleSubCategoryClick = (subCategoryId: number) => {
+    setSelectedSubCategory(subCategoryId);
+    onSubCategoryChange(subCategoryId);
   };
+
+  if (!loaded) {
+    return <div className="text-center">Loading...</div>;
+  }
 
   return (
-    <div className="justify-around hidden lg:flex">
-      {categories.map((category: any) => (
-        <><div className="flex flex-col items-center">
-          <div key={category.id} className={``}>
-            <button
-              type="button"
-              value={category.id}
-              onClick={() => handleCategoryChange(category.id)}
-            >
-              <div className="text-center flex flex-col items-center">
-                <div className="bg-gradient-to-b from-firstStepGradient via-secondStepGradient to-thirdStepGradient p-[2px] rounded-full">
-                  <img src={category.image} alt={category.name} className="rounded-full w-20 h-20" />
+    <div className="flex justify-center items-center overflow-y-scroll">
+      {data.map((category) => (
+        <div key={category.id} className="flex justify-center items-center flex-col gap-3">
+          <div
+            id={String(category.id)}
+            onClick={() => handleCategoryClick(category.id)}
+            className={`${isClicked && selectedCategory !== category.id ? "hidden" : "flex"} flex-col justify-center items-center gap-3 text-center w-128 p-5 cursor-pointer`}
+          >
+            <div className="bg-gradient-to-b from-firstStepGradient via-secondStepGradient to-thirdStepGradient p-1 rounded-full">
+              <img
+                src={category.image}
+                alt=""
+                className="h-28 w-auto rounded-full"
+              />
+            </div>
+            <h2 className="text-sm font-Luciole_Regular mt-4 text-nowrap text-center">{category.name}</h2>
+          </div>
+          <div
+            className={`${isClicked && selectedCategory === category.id ? "flex" : "hidden"}`}
+          >
+            {category.id_sub_cat.map((sub_cat) => (
+              <div key={sub_cat.Sub_category.id} className="flex flex-col justify-center items-center text-center w-32 cursor-pointer" onClick={() => handleSubCategoryClick(sub_cat.Sub_category.id)}>
+                <div className="bg-gradient-to-b from-firstStepGradient via-secondStepGradient to-thirdStepGradient p-1 rounded-full">
+                  <img
+                    src={sub_cat.Sub_category.image}
+                    alt=""
+                    className="h-20 w-auto rounded-full"
+                  />
                 </div>
-                <h2 className="text-sm font-Luciole_Regular mt-4 text-nowrap text-center">{category.name}</h2>
+                <h2 className="text-sm font-Luciole_Regular mt-4 text-nowrap text-center">{sub_cat.Sub_category.name}</h2>
               </div>
-            </button>
+            ))}
           </div>
-
-          <div className="w-[100%]">
-            {selectedCategory === category.id && category.id_sub_cat && (
-              <div className="justify-around">
-                {category.id_sub_cat.map((subcatName: any) => (
-                  <button
-                    key={subcatName.Sub_category.id}
-                    type="button"
-                    value={subcatName.Sub_category.id}
-                    onClick={() => handleSubCategoryChange(subcatName.Sub_category.id)}
-                  >
-                    <div className="text-center flex flex-col items-center">
-                      <div className="bg-gradient-to-b from-firstStepGradient via-secondStepGradient to-thirdStepGradient p-[2px] rounded-full">
-                        <img src={subcatName.Sub_category.image} alt={subcatName.Sub_category.name} className="rounded-full w-20 h-20" />
-                      </div>
-                      <h2 className="text-sm font-Luciole_Regular mt-4 text-nowrap text-center">{subcatName.Sub_category.name}</h2>
-                    </div>
-                  </button>
-                ))}
-              </div>
-            )}
-          </div>
-        </div></>
+        </div>
       ))}
     </div>
   );
