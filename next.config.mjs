b1c83cb7ced38929c0ae5735b2ceea3de35ccb61
1Benjamin-D <desmet.benjamin20@orange.fr>
2024-04-17 14:06:54 +0200

    test2

--- next.config.mjs
+++ next.config.mjs
@@ -11,13 +11,13 @@ const nextConfig = {
       },
     ],
   },
-  // typescript: {
-  //   // !! WARN !!
-  //   // Dangerously allow production builds to successfully complete even if
-  //   // your project has type errors.
-  //   // !! WARN !!
-  //   ignoreBuildErrors: true,
-  // },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
